fc127ef7af6eae486e860b2137890d1d7b15cd72
Rishad Islam <mislam191134@bscse.uiu.ac.bd>
2022-11-26 22:32:56 +0600

    first update

---
+++ js/index.js
@@ -0,0 +1,5 @@
+function modalLoad(){
+    const authenticationModal = document.getElementById('authentication-modal');
+    authenticationModal.setAttribute('aria-hidden','false');
+    authenticationModal.classList.remove("hidden")
+}
